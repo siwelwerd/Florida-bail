04102f7ea6890c5dda80f78c3c09f9e111574444
Drew Lewis <drew.lewis@gmail.com>
2025-04-10 12:00:10 -0700

    Get table rows

--- scrape_Hillsborough.R
+++ scrape_Hillsborough.R
@@ -11,16 +11,6 @@ open_url("https://hover.hillsclerk.com/html/case/caseSearch.html#nav-DateFiled-t
 
 Sys.sleep(2)
 # Select Criminal cases from dropdown
-# Select "Starts With Name Search" from the dropdown
-#session |>
-#  find_element("caseCategory") |>
-#  elem_set_value("CR")
-
-elements <- session |> find_elements(class_name="caseCategoryClass")
-
-print(elements[[1]])
-
-
 session %>% 
   find_elements(class_name="caseCategoryClass") %>%
   {.[[1]]} %>%
@@ -28,6 +18,7 @@ session %>%
 
 Sys.sleep(0.5)
 
+#Select Felonies and Misdemeanors
 caseTypes <- c("FELONY ADDENDUM", 
   "FELONY ADMINISTRATIVE CASE",
   "FELONY BOND FORFEITURE",
@@ -59,11 +50,63 @@ session %>%
   elem_select(text=caseTypes)
 
 # Need to get dates... date picker gets in the way.
-#session %>% 
-#  find_element(id="dateFiledBefore") %>%
-#  elem_set_value("01/01/2020")
+#Attempt to set value to get datepicker to show up
+session %>% 
+  find_element(id="dateFiledAfter") %>%
+  elem_set_value("01/01/2020")
+#Year
+session %>%
+  find_elements(class="ui-datepicker-year") %>%
+  {.[[1]]} %>%
+  elem_select("2020")
+#Month
+session %>%
+  find_elements(class="ui-datepicker-month") %>%
+  {.[[1]]} %>%
+  elem_select("0")
+#Day
+session %>%
+  find_elements(class="ui-state-default") %>%
+  {.[[1]]} %>%
+  elem_click()
 
-Sys.sleep(5)
+#Attempt to set value to get datepicker to show up
+session %>% 
+  find_element(id="dateFiledBefore") %>%
+  elem_set_value("01/01/2020")
+#Year
+session %>%
+  find_elements(class="ui-datepicker-year") %>%
+  {.[[1]]} %>%
+  elem_select("2020")
+#Month
+session %>%
+  find_elements(class="ui-datepicker-month") %>%
+  {.[[1]]} %>%
+  elem_select("0")
+#Day
+session %>%
+  find_elements(class="ui-state-default") %>%
+  {.[[1]]} %>%
+  elem_click()
+
+#Click search button
+session %>%
+  find_element(id="btnSubmitDateFiledSearch") %>%
+  elem_scroll_to() %>%
+  elem_click()
+
+
+Sys.sleep(10)
+
+t <- session |>
+  find_element(id="mycasesdata") %>%
+  find_element("tbody") %>%
+  find_elements("tr")
+#
+#children <- t |> elem_children()
+#
+print(t)
 
 #source <- session |> get_page_source()
 
