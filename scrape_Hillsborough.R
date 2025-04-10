24aa2c15147264acbfc76ed2ae86e96f70e40398
Drew Lewis <drew.lewis@gmail.com>
2025-04-10 15:52:48 -0700

    Iterate over dates

--- scrape_Hillsborough.R
+++ scrape_Hillsborough.R
@@ -1,22 +1,11 @@
 # Load libraries
 library(tidyverse)
+library(lubridate)
 library(selenider)
 library(rvest)
 
 # Open selenium session
 session <- selenider_session("selenium",browser="chrome")
-
-# Navigate to page
-open_url("https://hover.hillsclerk.com/html/case/caseSearch.html#nav-DateFiled-tab")
-
-Sys.sleep(2)
-# Select Criminal cases from dropdown
-session %>% 
-  find_elements(class_name="caseCategoryClass") %>%
-  {.[[1]]} %>%
-  elem_select(text="CRIMINAL")
-
-Sys.sleep(0.5)
 
 #Select Felonies and Misdemeanors
 caseTypes <- c("FELONY ADDENDUM", 
@@ -44,70 +33,94 @@ caseTypes <- c("FELONY ADDENDUM",
   "MISDEMEANOR NON CRIMINAL INFRACTIONS",
   "MISDEMEANOR ORDINANCE VIOLATIONS")
 
-session %>%
-  find_elements(class_name="caseTypesClass") %>%
-  {.[[1]]} %>%
-  elem_select(text=caseTypes)
+startDate = ymd("2020-01-01")
+endDate = ymd("2020-12-31")
 
-# Need to get dates... date picker gets in the way.
-#Attempt to set value to get datepicker to show up
-session %>% 
-  find_element(id="dateFiledAfter") %>%
-  elem_set_value("01/01/2020")
-#Year
-session %>%
-  find_elements(class="ui-datepicker-year") %>%
-  {.[[1]]} %>%
-  elem_select("2020")
-#Month
-session %>%
-  find_elements(class="ui-datepicker-month") %>%
-  {.[[1]]} %>%
-  elem_select("0")
-#Day
-session %>%
-  find_elements(class="ui-state-default") %>%
-  {.[[1]]} %>%
-  elem_click()
+totalCases=0
 
-#Attempt to set value to get datepicker to show up
-session %>% 
-  find_element(id="dateFiledBefore") %>%
-  elem_set_value("01/01/2020")
-#Year
-session %>%
-  find_elements(class="ui-datepicker-year") %>%
-  {.[[1]]} %>%
-  elem_select("2020")
-#Month
-session %>%
-  find_elements(class="ui-datepicker-month") %>%
-  {.[[1]]} %>%
-  elem_select("0")
-#Day
-session %>%
-  find_elements(class="ui-state-default") %>%
-  {.[[1]]} %>%
-  elem_click()
+#Loop over dates in range, we will fetch one day at a time
+for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
+{
+  # Navigate to page
+  open_url("https://hover.hillsclerk.com/html/case/caseSearch.html#nav-DateFiled-tab")
+  
+  # Select Criminal cases from dropdown
+  Sys.sleep(1)
+  session %>% 
+    find_element(class_name="caseCategoryClass") %>%
+    elem_expect(is_present) %>%
+    elem_select(text="CRIMINAL")
+  
+  Sys.sleep(0.5)
+  session %>%
+    find_element(class_name="caseTypesClass") %>%
+    elem_select(text=caseTypes)
+  
+  # Need to set dates... date picker gets in the way.
+  #Attempt to set value to get datepicker to show up
+  session %>% 
+    find_element(id="dateFiledAfter") %>%
+    elem_set_value("01/01/2020")
+  #Select Year
+  session %>%
+    find_elements(class="ui-datepicker-year") %>%
+    {.[[1]]} %>%
+    elem_select(year(d))
+  #Select Month
+  session %>%
+    find_elements(class="ui-datepicker-month") %>%
+    {.[[1]]} %>%
+    elem_select(month(d)-1)
+  #Select Day
+  session %>%
+    find_elements(class="ui-state-default") %>%
+    {.[[mday(d)]]} %>%
+    elem_click()
+  
+  #Attempt to set value to get datepicker to show up
+  session %>% 
+    find_element(id="dateFiledBefore") %>%
+    elem_set_value("01/01/2020")
+  #Set Year
+  session %>%
+    find_elements(class="ui-datepicker-year") %>%
+    {.[[1]]} %>%
+    elem_select(year(d))
+  #Set Month
+  session %>%
+    find_elements(class="ui-datepicker-month") %>%
+    {.[[1]]} %>%
+    elem_select(as.character(month(d)-1))
+  #Set Day
+  session %>%
+    find_elements(class="ui-state-default") %>%
+    {.[[mday(d)]]} %>%
+    elem_click()
+  
+  #Click search button
+  session %>%
+    find_element(id="btnSubmitDateFiledSearch") %>%
+    elem_scroll_to() %>%
+    elem_click()
+  
 
-#Click search button
-session %>%
-  find_element(id="btnSubmitDateFiledSearch") %>%
-  elem_scroll_to() %>%
-  elem_click()
+  #Each case is in a table row, so pull those out,
+  #But make sure we wait for it to load.
+  #We get an empty table row while loading, so wait for length to be
+  #more than 2
+  t <- session |>
+    find_element(id="mycasesdata") %>%
+    find_element("tbody") %>%
+    find_elements("tr") %>%
+    elem_expect(
+    \(elem) elem |>
+      length() >2 
+    )
+  
+  totalCases <- totalCases + length(t)
+  print(paste("There were", length(t), "cases on", d))
+  
+}
 
+print(paste("There were", totalCases, "total cases between", startDate,"and",endDate))
 
-Sys.sleep(10)
-
-t <- session |>
-  find_element(id="mycasesdata") %>%
-  find_element("tbody") %>%
-  find_elements("tr")
-#
-#children <- t |> elem_children()
-#
-print(t)
-
-#source <- session |> get_page_source()
-
-#print(source %>% xml_structure())
--- test.R
+++ test.R
@@ -1,8 +1,18 @@
 library(selenider)
 library(rvest)
+library(lubridate)
 
-session <- selenider_session("selenium",browser="chrome")
-open_url("https://appsgp.mypalmbeachclerk.com/eCaseView")
+#session <- selenider_session("selenium",browser="chrome")
+#open_url("https://appsgp.mypalmbeachclerk.com/eCaseView")
+#
+#rv <- session |> get_page_source() |> html_table() 
+#print(rv[[1]])
 
-rv <- session |> get_page_source() |> html_table() 
-print(rv[[1]])
+
+startDate = ymd("2020-01-01")
+endDate = ymd("2020-01-03")
+
+for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
+{
+  print(d)
+}
