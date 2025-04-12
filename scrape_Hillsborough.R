d8ed7442bffab71c24907b8f28b58f89844c720d
Drew Lewis <drew.lewis@gmail.com>
2025-04-12 16:30:13 -0700

    Finally finished running

--- scrape_Hillsborough.R
+++ scrape_Hillsborough.R
@@ -33,16 +33,47 @@ caseTypes <- c("FELONY ADDENDUM",
   "MISDEMEANOR NON CRIMINAL INFRACTIONS",
   "MISDEMEANOR ORDINANCE VIOLATIONS")
 
-startDate = ymd("2020-01-01")
-endDate = ymd("2020-12-31")
+fileName = "Hillsborough counts.csv"
 
-totalCases=0
+startDate <- ymd("2020-01-01")
+endDate <- ymd("2024-12-31")
+
+
+#Check for file from aborted past run, and adjust date to pickup
+#where we left off if needed
+if( file.exists(fileName) )
+{
+  df <- read_table(fileName)
+  lastDate <- df %>% tail(n=1) %>% {.[[1,1]]} %>% ymd()
+  if(lastDate > startDate)
+  {
+    startDate <- lastDate + 1
+    print(paste("Resuming from ",lastDate))
+  }
+  if(lastDate == endDate)
+  {
+    print(paste("There were", sum(df$cases), "total cases between ", df[[1,1]], "and",endDate)) 
+    quit()
+  }
+} else
+{
+  df <- tibble(date= Date(), cases=numeric())
+}
 
 #Loop over dates in range, we will fetch one day at a time
 for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
 {
+  print(d)
+  
+  #Initial waiting period before retrying
+  cooloff=2
+  #Wrap in a while loop so we can retry  the same date if it errors out
+  while(TRUE){
   # Navigate to page
   open_url("https://hover.hillsclerk.com/html/case/caseSearch.html#nav-DateFiled-tab")
+  
+  #Add some sleepy time to maybe reduce errors
+  Sys.sleep(cooloff)
   
   # Select Criminal cases from dropdown
   Sys.sleep(1)
@@ -51,7 +82,7 @@ for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
     elem_expect(is_present) %>%
     elem_select(text="CRIMINAL")
   
-  Sys.sleep(0.5)
+  Sys.sleep(1.5)
   session %>%
     find_element(class_name="caseTypesClass") %>%
     elem_select(text=caseTypes)
@@ -104,23 +135,46 @@ for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
     elem_click()
   
 
+  
+
   #Each case is in a table row, so pull those out,
   #But make sure we wait for it to load.
   #We get an empty table row while loading, so wait for length to be
   #more than 2
-  t <- session |>
+  #This keeps erroring, so wrap it in a try
+  t <- try(session |>
     find_element(id="mycasesdata") %>%
     find_element("tbody") %>%
     find_elements("tr") %>%
     elem_expect(
     \(elem) elem |>
       length() >2 
-    )
+    ,timeout=10) 
+  )
+  if(!inherits(t,'try-error'))
+  {
+    break
+  } else
+  {
+    #Wait for a bit and then try again.  Increment wait time each attempt
+    print(paste("Error on ",d, " trying again"))
+    Sys.sleep(2)
+    #Sometimes this fails too, so wrap it in a try
+    try(close_session())
+    Sys.sleep(cooloff)
+    session <- selenider_session("selenium",browser="chrome")
+    cooloff <- cooloff+30
+    next
+  }
+  }
   
-  totalCases <- totalCases + length(t)
-  print(paste("There were", length(t), "cases on", d))
+  caseCount <- length(t)
+  
+  print(paste("There were", caseCount, "cases on", d))
+  df <- df %>% add_row(date=d,cases=caseCount)
+  df %>% write.table(file="Hillsborough counts.csv",row.names=FALSE,quote=FALSE)
   
 }
 
-print(paste("There were", totalCases, "total cases between", startDate,"and",endDate))
+print(paste("There were", sum(df$cases), "total cases."))
 
--- test.R
+++ test.R
@@ -1,6 +1,8 @@
 library(selenider)
 library(rvest)
+library(tidyverse)
 library(lubridate)
+
 
 #session <- selenider_session("selenium",browser="chrome")
 #open_url("https://appsgp.mypalmbeachclerk.com/eCaseView")
@@ -9,10 +11,26 @@ library(lubridate)
 #print(rv[[1]])
 
 
-startDate = ymd("2020-01-01")
-endDate = ymd("2020-01-03")
+startDate = ymd("2020-03-25")
+endDate = ymd("2020-04-03")
+dates = as.list(seq(startDate,endDate,as.difftime(days(1))))
 
-for (d in as.list(seq(startDate,endDate,as.difftime(days(1)))))
+
+df <- read_table("Hillsborough counts.csv")
+for (d in dates)
 {
-  print(d)
+  #print(d)
+  #df <- df %>% add_row(date=format_ISO8601(d),cases=10)
+}
+
+print(df)
+last_date <- df %>% tail(n=1) %>% {.[[1,1]]}
+print(last_date)
+if (ymd(last_date) > startDate)
+{
+  print ("Yes1")
+}
+if (ymd(last_date) > endDate)
+{
+  print ("Yes2")
 }
